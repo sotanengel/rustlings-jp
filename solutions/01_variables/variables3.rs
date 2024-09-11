2b47e5d245903bc4dcca1c3e4cfba4ec43be46e8
sota.n <sota.n.eng@gmail.com>
2024-09-11 09:45:26 +0900

    variables3

--- solutions/01_variables/variables3.rs
+++ solutions/01_variables/variables3.rs
@@ -1,4 +1,15 @@
+#![allow(clippy::needless_late_init)]
+
 fn main() {
-    // DON'T EDIT THIS SOLUTION FILE!
-    // It will be automatically filled after you finish the exercise.
+    // Rustでは初期化されていない変数を読み込むことは認められていません。
+    // したがって最初に変数に値を代入する必要があります。
+    let x: i32 = 42;
+
+    println!("Number {x}");
+
+    // 変数の宣言後に初期化をすることも可能です。
+    // しかしその変数を初期化前に使うことはできません。
+    let y: i32;
+    y = 42;
+    println!("Number {y}");
 }
